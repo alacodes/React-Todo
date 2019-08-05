d4eb4893ffb424886c1b2472a8e7f3049b46b7f6
Tracie T <traciecodes@gmail.com>
2019-08-05 15:38:17 -0500

    Initial commit: Yarn install plus set-up of tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React from 'react';
 
+
+const taskData = [
+  {
+    name: 'Go to the gym', 
+    id: 500,
+    completed: false
+  },
+  {
+    name: 'Set up mentor meeting', 
+    id: 150,
+    completed: false
+  },
+  {
+    name: 'Take notes for tomorrow', 
+    id: 417,
+    completed: false
+  },
+  {
+    name: 'Clean the floor', 
+    id: 205,
+    completed: false
+  },
+  {
+    name: 'Groceries + meal prep', 
+    id: 478,
+    completed: false
+  },
+  {
+    name: 'Work on resume formatting', 
+    id: 312,
+    completed: false
+  }
+];
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: taskData
+    };
+  }
+
   render() {
     return (
       <div>
@@ -14,3 +52,11 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
+
+//DIRECTIONS: 
+  // you will need a place to store your state in this component.
+  // design `App` to be the parent component of your application.
+  // this component is going to take care of state, and any change handlers you need to work with your state
